6166df2e8ebc510b8b3a8849a1572a9b371e4d96
Sadik Ay <sadikay91@gmail.com>
2014-11-09 00:27:41 +0200

    completed admin panel

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -2,8 +2,43 @@ class Admin::ArticlesController < Admin::BaseAdminController
   def index
     @articles=Article.all
   end
+
+  def create
+    @article= Article.new article_params
+    if @article.save
+      flash[:notice]='Makale başarı ile eklendi'
+      redirect_to admin_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @articles = Article.find(params[:id])
     @category= Category.all
+  end
+
+  def new
+    @articles= Article.new
+    @category= Category.all
+  end
+
+  def update
+    @article= Article.find params[:id]
+    @category= Category.find params[:id]
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to admin_path, notice: 'Makale Güncellendi' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit!
   end
 end
--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -17,7 +17,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process :resize_to_fit => [125, 50]
+    #process :resize_to_fit => [125, 50]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
