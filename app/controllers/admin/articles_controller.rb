b2eeb33c09567463a572bb5457cfa7344e8522b6
Sadik Ay <sadikay91@gmail.com>
2014-11-08 18:48:02 +0200

    added edit of articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -2,4 +2,8 @@ class Admin::ArticlesController < Admin::BaseAdminController
   def index
     @articles=Article.all
   end
+  def edit
+    @articles = Article.find(params[:id])
+    @category= Category.all
+  end
 end
