72d83d1867d6a4d8e1421fd0c05f89b11c345c08
Sadik Ay <sadikay91@gmail.com>
2014-12-01 03:21:46 +0200

    added a future about photo gallery

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
       @category = Category.find params[:id]
+      @images = Image.where.not("id = 24 OR id = 27 OR id = 28")
   end
 end
